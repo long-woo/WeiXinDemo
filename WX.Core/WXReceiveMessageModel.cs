8b95cb87b0a16d8241449e047ecb75c47670a4a8
JasonLong-7 <longwunet@gmail.com>
2015-11-05 22:03:40 +0800

    增加接收微信消息实体类

---
+++ WX.Core/WXReceiveMessageModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WX.Core
+{
+
+    /// <summary>
+    /// 接收微信消息实体对象
+    /// </summary>
+    public class WXReceiveMessageModel
+    {
+        /// <summary>
+        /// 开发者微信号
+        /// </summary>
+        public string ToUserName { get; set; }
+
+        /// <summary>
+        /// 发送方帐号（一个OpenID）
+        /// </summary>
+        public string FromUserName { get; set; }
+
+        /// <summary>
+        /// 消息创建时间 （整型）
+        /// </summary>
+        public string CreateTime { get; set; }
+
+        /// <summary>
+        /// 消息类型
+        /// </summary>
+        public string MsgType { get; set; }
+
+        /// <summary>
+        /// 事件类型
+        /// </summary>
+        public string Event { get; set; }
+    }
+}
