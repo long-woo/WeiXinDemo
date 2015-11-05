7be45ebcaf704bc957a8fc77cc3adf731f9a12e1
JasonLong-7 <longwunet@gmail.com>
2015-11-05 22:12:58 +0800

    提交遗漏文件（WXApi.cs、WXLog.cs）

---
+++ WX.Core/WXApi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using System.Web.Configuration;
+
+namespace WX.Core
+{
+    public class WXApi
+    {
+        /// <summary>
+        /// 获取Access Token
+        /// </summary>
+        /// <param name="appId">应用Id</param>
+        /// <param name="appSecret">应用密钥</param>
+        /// <returns></returns>
+        public static async Task<JObject> GetAccessTokenAsync(string appId, string appSecret)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            string strUrl = string.Format(WebConfigurationManager.AppSettings["ACCESStOKEN"] + "grant_type=client_credential&appid={0}&secret={1}", appId, appSecret);
+            var strUri = new Uri(strUrl);
+            string result = await httpClient.GetStringAsync(strUri);
+
+            return JObject.Parse(result);
+        }
+    }
+}
---
+++ WX.Core/WXLog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WX.Core
+{
+    public class WXLog
+    {
+        public void WriteLog(string text)
+        {
+            string htmlTemp="<!DOCTYPE html><html lang=\"zh-cn\"><head><meta charset=\"utf-8\"/><title>微信日志记录</title></head><body><ul></ul></body></html>";
+            const string foldName = "wxlog";
+            string targetName = DateTime.Now.ToString("yyyy-MM-dd");
+            string fileName = DateTime.Now.ToString("yyyyMMddHH:mm:ss");
+            string logPath = HttpContext.Current.Server.MapPath(string.Format("~/{0}/{1}", foldName, targetName));
+            if (!Directory.Exists(logPath))
+            {
+                Directory.CreateDirectory(logPath);
+            }
+
+            if (!File.Exists(fileName + ".html"))
+            {
+                File.CreateText(fileName + ".html").Close();
+            }
+            using (StreamWriter writer = new StreamWriter(logPath + fileName + ".html", false, Encoding.UTF8))
+            {
+                writer.BaseStream.Seek(0, SeekOrigin.Begin);
+                writer.Write(text);
+            }
+        }
+    }
+}
